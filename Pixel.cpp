0c214a3998e007e69b01d9d4366a18b1262d535f
Maakinoh <wiispieler24@gmail.com>
2018-04-05 19:35:30 +0200

    .

--- Pixel.cpp
+++ Pixel.cpp
@@ -1,20 +1,29 @@
-#include <Pixel.h>
-#include <Adafruit_NeoPixel.h>
-#include <Animation.h>
+#include "Pixel.h"
+#include "Adafruit_NeoPixel.h"
 
 
 Adafruit_NeoPixel strip;
+int animationCount;
+
 Pixel::Pixel(int pin,int pixelCount) {
     strip = Adafruit_NeoPixel(PIN_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+    strip.begin();
+ strip.setBrightness(20);
+ strip.show();
 }
 
 
-void Pixel::setAnimation(){
+
+void Pixel::setAnimation(int animationCode){
 
 }
 
 
 void Pixel::update(){
 
+}
 
+
+void on(int pixel, uint32_t color ){
+  strip.setPixelColor(pixel,color);
 }
--- Pixel.h
+++ Pixel.h
@@ -1,14 +1,13 @@
-#ifndef Pixel_h
-#define Pixel_h
+#ifndef Pixel_h_
+#define Pixel_h_
 #include <Adafruit_NeoPixel.h>
 // the #include statment and code go here...
 
 class Pixel{
 public:
     Pixel(int pin, int pixelCount);
-    void setAnimation();
+    void setAnimation(int animationCode);
     void update();
-
 };
 
 #endif
